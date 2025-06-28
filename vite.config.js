c16930c33e21988cbe01d4072dba1eb3c946ecfc
hussain <shaikhussain2402@gmail.com>
2025-06-28 17:52:32 +0530

    fix:add base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // <-- this fixes blank screen on deployment
+  base: '/',  // <-- this fixes blank screen on deployment
 });
